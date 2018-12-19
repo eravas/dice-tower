bd79a7cdedce394b159491bb10c40ed82fcf6131
Harsh Patel <harshnp02@gmail.com>
2018-12-19 17:22:25 -0500

    Update tower.js

--- tower.js
+++ tower.js
@@ -18,8 +18,8 @@ function varSet() {
           case 3:
                document.getElementById("rngQuote").value="You succesfully dodge the air next to the enemy's fist...unfortunately the fist hurts more";
                break;*/
-     if (n > 999 || n < 1) { 
-          alert("By decree of Bahamut the wise, in all his wisdom, rolling more than 999 or less than 1 dice is forbidden at this time."); 
+     if (n > 9999 || n < 1) { 
+          alert("By decree of Bahamut the wise, in all his wisdom, rolling more than 9999 or less than 1 dice is forbidden at this time."); 
      } else { 
           dTower(s,n); 
      }   
@@ -30,9 +30,9 @@ function dTower(sides,number) {
      var critCheck = document.getElementById("critBox").checked;
      for (i = 1; i <= number; i++) {
           var roll = Math.floor(Math.random() * (sides)) + 1;
-          output += "+" + roll + "";
+          output += roll + ", ";
           sum += roll;
-          if (i % 10 == 0) {
+          if (i % 15 == 0) {
                output += "\n";
           } 
      }
@@ -40,11 +40,14 @@ function dTower(sides,number) {
           var critDmg = sum*2;
           var maxQ = document.getElementById("numberDice").value * document.getElementById("sides").value
           if(sum == maxQ) {
-               document.getElementById("result").value="2 x (" + output + ") = " + critDmg + " Now that's a lot of damage!"
+               document.getElementById("total").value="2 x (" + sum + ") = " + critDmg + "Now that's a lot of damage!"
+               document.getElementById("result").value=output
           } else {
-               document.getElementById("result").value="2 x (" + output + ") = " + critDmg + ""
+               document.getElementById("total").value="2 x (" + sum + ") = " + critDmg
+               document.getElementById("result").value=output
           }
      } else {
-          document.getElementById("result").value=output + "=" + sum + ""
+          document.getElementById("total").value=sum
+          document.getElementById("result").value=output
      }
 }
