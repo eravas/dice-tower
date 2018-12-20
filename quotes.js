a524a2df1a4e7c6d45d1a9cc5eb947fe9accf8af
Harsh Patel <harshnp02@gmail.com>
2018-12-19 21:23:23 -0500

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,1 +1,33 @@
-//this script will handle the randomized messages and remarks in the future
+//this script will handle the randomized messages and remarks
+/*function getQuote {
+  var rng = Math.floor(Math.random() * 3) + 1;
+  switch (rng) {
+    case 1:
+      document.getElementById("rngQuote").value="You visciously tap the enemy";
+      break;
+    case 2:
+      document.getElementById("rngQuote").value="You land a powerful strike with deadly precision...on the enemy's shield";
+      break;
+    case 3:
+      document.getElementById("rngQuote").value="You succesfully dodge the air next to the enemy's fist...unfortunately the fist hurts more";
+      break;
+    case 4:
+      document.getElementById("rngQuote").value="It is not the bee that kills, but the swarm";
+      break;
+    case 5:
+      document.getElementById("rngQuote").value="you display a show of strength. It wasn't much";
+      break;
+    case 6:
+      document.getElementById("rngQuote").value="you stab the enemy....while holding the blade backwards.";
+      break;
+    case 7:
+      document.getElementById("rngQuote").value="You swing at the enemy. They get a nice taste of the handle";
+      break;
+    case 8:
+      document.getElementById("rngQuote").value="You throw your spear... backwards";
+      break;
+    default:
+      document.getElementById("rngQuote").value="Take a chance roll the dice";
+      break;
+  }
+}*/
