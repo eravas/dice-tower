a12f374b27e842a46cf458f7673a65ee6ea842ac
Harsh Patel <harshnp02@gmail.com>
2018-12-19 21:11:37 -0500

    Update tower.js

--- tower.js
+++ tower.js
@@ -1,23 +1,9 @@
-//all of the js code
+//most of the js code
 var s = 8;
 var n = 6;
 function varSet() {
      s = document.getElementById("sides").value;
      n = document.getElementById("numberDice").value;
-     //var rng = Math.floor(Math.random() * 3) + 1;
-     /*switch (rng) {
-          default:
-               document.getElementById("rngQuote").value="Take a chance roll the dice";
-               break;
-          case 1:
-               document.getElementById("rngQuote").value="You visciously tap the enemy";
-               break;
-          case 2:
-               document.getElementById("rngQuote").value="You land a powerful strike with deadly precision...on the enemy's shield";
-               break;
-          case 3:
-               document.getElementById("rngQuote").value="You succesfully dodge the air next to the enemy's fist...unfortunately the fist hurts more";
-               break;*/
      if (n > 9999 || n < 1) { 
           alert("By decree of Bahamut the wise, in all his wisdom, rolling more than 9999 or less than 1 dice is forbidden at this time."); 
      } else { 
