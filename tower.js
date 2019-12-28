e2e0e4b6bc27fb1064f622e48cdc6818b59ea72d
Harsh Patel <harshnp02@gmail.com>
2019-12-27 21:12:52 -0500

    Update tower.js

--- tower.js
+++ tower.js
@@ -28,12 +28,11 @@ function rollSet() {
      }
      rollResMod += rollRes;
      if (rollResMod <= 0) { 
-          document.getElementById("result").value = 0;
           document.getElementById("total").value = 0;
-     } else {
-          document.getElementById("result").value = rollRes + " + " + document.getElementById("setMods").value;
+     } else { 
           document.getElementById("total").value = rollResMod;
      }
+     document.getElementById("result").value = rollRes + " + " + document.getElementById("setMods").value;
 }
 
 //a seperate function used for rolling stats in D&D fifth edition via a method selected by the user via a drop-down selection
